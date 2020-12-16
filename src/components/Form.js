bcbf559bd5b02066c650fe68ecd6a13dfcd28660
tiffany <yang.ding@hotmail.com>
2020-12-16 14:57:51 -0800

    added form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,31 +9,44 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name"
-        ref={register}
-      />
-      <input
-        type="text"
-        name="lastName"
-        placeholder="Last Name"
-        ref={register}
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Email Address"
-        ref={register}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        ref={register}
-      />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          ref={register({ required: true })}
+        />
+        {errors.firstName && <p className="error">Field cannot be empty</p>}
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          ref={register({ required: true })}
+        />
+        {errors.lastName && <p className="error">Field cannot be empty</p>}
+      </div>
+      <div className="form-group">
+        <input
+          name="email"
+          placeholder="Email Address"
+          ref={register({ required: true })}
+        />
+        {errors.email && <p className="error">Invalid e-mail address</p>}
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          ref={register({ required: true, minLength: 8 })}
+        />
+        {errors.password && (
+          <p className="error">Password must contain at least 8 characters</p>
+        )}
+      </div>
       <button type="submit">CLAIM YOUR FREE TRIAL</button>
       <p>
         By clicking the button, you are agreeing to our{' '}
